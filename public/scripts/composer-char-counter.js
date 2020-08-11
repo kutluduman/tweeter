b56a43b86b61640ce72b19ef8d0ed3f8bfb139f6
Kutluhan Duman <kutluduman@gmail.com>
2020-08-11 22:22:15 +0000

    The counter is working as expected. Fixed the counter function and the color issue from css file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,19 @@
 $(document).ready(() => {
+ const counter = $('.counter')[0];
+ const textArea = $('textarea');
 
 
+ $(textArea).on('input', function () {
+  let textCount = $(this).val();
+  $(counter).text(140 - textCount.length);
 
+  if (textCount.length > 140) {
+    $(counter).addClass('redLimit');
+  } else {
+    $(counter).removeClass('redLimit');
+  }
+
+
+ });
+ 
 });

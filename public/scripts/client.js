9edd0c236a3dd8a6640b35b39cb0ba1d2d784a91
Kutluhan Duman <kutluduman@gmail.com>
2020-08-12 17:53:56 +0000

    Renders and appends the data from the array and usses the values in the html file with the help of jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,40 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Descartes",
-    "avatars": "https://i.imgur.com/nlhLi3I.png",
-    "handle": "@rd" },
-  "content": {
-    "text": "Je pense , donc je suis"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "created_at": 1461113959088
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 
 $(document).ready(function () {
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
 
   const createTweetElement = (tweet) => {
     const tweetDate = new Date(tweet.created_at);
@@ -45,8 +66,7 @@ $(document).ready(function () {
 
   };
 
-  const $tweet = createTweetElement(tweetData);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
   
-
 });
+

7902017621a91d650540bf4b5ed03cfdf4070b04
Kutluhan Duman <kutluduman@gmail.com>
2020-08-12 19:06:14 +0000

    Finished implementing loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 $(document).ready(function () {
 
-  const getTweets = () => {
+  const loadtweets = () => {
     $.ajax('/tweets/', { method: 'GET'})
     .then(function(response) {
       renderTweets(response);
@@ -74,14 +47,16 @@ $(document).ready(function () {
 
   };
 
-  $('#submit-tweet').submit(function (event) {
+  loadtweets();
+
+  $('#submit-tweet').submit(function(event) {
     event.preventDefault();
     const formData = ($(this).serialize());
 
     $.ajax('/tweets/', { method: 'POST', data: formData })
     .then(function(response) {
       console.log('Response: ', response);
-    })
+    });
 
   });
   

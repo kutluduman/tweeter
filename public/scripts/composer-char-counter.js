3e1fabbc76de557347d19d5a92f78614a867476c
Kutluhan Duman <kutluduman@gmail.com>
2020-08-12 13:30:42 +0000

    Lint fixes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 $(document).ready(() => {
- const counter = $('.counter')[0];
- const textArea = $('textarea');
+  const counter = $('.counter')[0];
+  const textArea = $('textarea');
 
 
- $(textArea).on('input', function () {
-  let textCount = $(this).val();
-  $(counter).text(140 - textCount.length);
+  $(textArea).on('input', function() {
+    let textCount = $(this).val();
+    $(counter).text(140 - textCount.length);
 
-  if (textCount.length > 140) {
-    $(counter).addClass('redLimit');
-  } else {
-    $(counter).removeClass('redLimit');
-  }
+    if (textCount.length > 140) {
+      $(counter).addClass('redLimit');
+    } else {
+      $(counter).removeClass('redLimit');
+    }
 
 
- });
+  });
  
 });
